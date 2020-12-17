237a5f16ea464e91533d55a5325b9b857b755f8b
Efrain May <wwifihack@gmail.com>
2020-12-16 19:47:25 -0600

    se termino la aplicacion agregandole la grafica con chat js actualizando los datos en tiempo real, aplicacion de votos lista

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import { React, useContext } from "react";
 import { BandAdd } from "../components/BandAdd";
+import { BandChart } from "../components/BandChart";
 import { BandList } from "../components/BandList";
 import { SocketContext } from '../context/SocketContext';
 
@@ -22,6 +23,12 @@ function HomePage() {
 
       <h1>Band Names</h1>
       <hr/>
+
+      <div className="row">
+        <div className="col">
+          <BandChart />
+        </div>
+      </div>
 
       <div className="row">
         <div className="col-8">
